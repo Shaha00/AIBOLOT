a292cf8da2919023f10974b17aed29c587df1400
Shaha00 <ahmadoshahzod438@gmail.com>
2023-01-23 23:02:19 +0600

    Accerted

--- handlers/client.py
+++ handlers/client.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import bot, dp
-from keyboards.client_kb import start_markup
+from keyboards.client_kb import start_markup, direction_markup
+from aiogram.dispatcher.filters import Text
 
 
 async def start_handler(message: types.Message):
@@ -10,5 +11,10 @@ async def start_handler(message: types.Message):
                            reply_markup=start_markup)
 
 
+async def client_button1(message: types.Message):
+    await bot.send_message(message.from_user.id, reply_markup=direction_markup, text='Выберите направление!')
+
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(start_handler, commands=['start', 'help'])
+    dp.register_message_handler(client_button1, commands=['record'])
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -3,8 +3,26 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 start_markup = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True,
+    row_width=3
 )
 
 start_button = KeyboardButton("/start")
+button1 = KeyboardButton('/record')
+button2 = KeyboardButton('Информация')
+button3 = KeyboardButton('Наша Геолокация')
 
-start_markup.add(start_button)
+
+start_markup.add(button1, button2, button3)
+
+direction_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    KeyboardButton("Хирург"),
+    KeyboardButton("Терапевт"),
+    KeyboardButton("Уролог"),
+    KeyboardButton("Гинеколог"),
+    KeyboardButton("Стоматолог"),
+    KeyboardButton("Окулист"),
+    KeyboardButton("Травматолог"),
+)
